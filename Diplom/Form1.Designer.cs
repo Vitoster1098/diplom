7054ee1c5c48a6ccccb573ed5b7c6321d822adb7
Vitoster1098 <zzzvitzzz4@gmail.com>
2021-10-31 21:02:38 +0300

    Git init

---
+++ Diplom/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace Diplom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connect = new System.Windows.Forms.Button();
+            this.disconnect = new System.Windows.Forms.Button();
+            this.connect_status = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.default_connection = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.фотоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьВсёToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.connect_status)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connect
+            // 
+            this.connect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.connect.Location = new System.Drawing.Point(3, 3);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(49, 23);
+            this.connect.TabIndex = 0;
+            this.connect.Text = "db con";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // disconnect
+            // 
+            this.disconnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.disconnect.Location = new System.Drawing.Point(58, 3);
+            this.disconnect.Name = "disconnect";
+            this.disconnect.Size = new System.Drawing.Size(47, 23);
+            this.disconnect.TabIndex = 1;
+            this.disconnect.Text = "db dis";
+            this.disconnect.UseVisualStyleBackColor = true;
+            this.disconnect.Click += new System.EventHandler(this.disconnect_Click);
+            // 
+            // connect_status
+            // 
+            this.connect_status.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.connect_status.BackColor = System.Drawing.Color.Red;
+            this.connect_status.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.connect_status.Location = new System.Drawing.Point(111, 3);
+            this.connect_status.Name = "connect_status";
+            this.connect_status.Size = new System.Drawing.Size(23, 23);
+            this.connect_status.TabIndex = 2;
+            this.connect_status.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.default_connection);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.connect);
+            this.panel1.Controls.Add(this.connect_status);
+            this.panel1.Controls.Add(this.disconnect);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 400);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(701, 32);
+            this.panel1.TabIndex = 3;
+            // 
+            // default_connection
+            // 
+            this.default_connection.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.default_connection.Location = new System.Drawing.Point(620, 4);
+            this.default_connection.Name = "default_connection";
+            this.default_connection.Size = new System.Drawing.Size(75, 23);
+            this.default_connection.TabIndex = 4;
+            this.default_connection.Text = "def con";
+            this.default_connection.UseVisualStyleBackColor = true;
+            this.default_connection.Click += new System.EventHandler(this.default_connection_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(140, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(474, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.очиститьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(701, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.фотоToolStripMenuItem});
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            // 
+            // фотоToolStripMenuItem
+            // 
+            this.фотоToolStripMenuItem.Name = "фотоToolStripMenuItem";
+            this.фотоToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.фотоToolStripMenuItem.Text = "Папку";
+            this.фотоToolStripMenuItem.Click += new System.EventHandler(this.фотоToolStripMenuItem_Click);
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.очиститьВсёToolStripMenuItem});
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            // 
+            // очиститьВсёToolStripMenuItem
+            // 
+            this.очиститьВсёToolStripMenuItem.Name = "очиститьВсёToolStripMenuItem";
+            this.очиститьВсёToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.очиститьВсёToolStripMenuItem.Text = "Очистить всё";
+            this.очиститьВсёToolStripMenuItem.Click += new System.EventHandler(this.очиститьВсёToolStripMenuItem_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(0, 385);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(701, 15);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Список изображений:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(10, 61);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(170, 303);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(186, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(242, 303);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(434, 61);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(242, 303);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Изображение локально:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(431, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Изображение из БД:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 432);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Анализ фото";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.connect_status)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button disconnect;
+        private System.Windows.Forms.PictureBox connect_status;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button default_connection;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem фотоToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьВсёToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Diplom/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Diplom
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string progpath = AppDomain.CurrentDomain.BaseDirectory;
+        string connectionStringDefault = "";
+        string connectionString = @"", path = "";
+        private OleDbConnection connection;
+        string[] exsamplesPath;
+        OleDbCommand dbCommand = new OleDbCommand();
+
+        private void connect_Click(object sender, EventArgs e) //Соединение с БД
+        {
+            makeConnection(textBox1.Text, connect_status);
+        }
+        public void makeConnection(string newconnectionString, PictureBox status)
+        {
+            try
+            {
+                connection = new OleDbConnection(newconnectionString);
+                connection.Open();
+                connectionString = newconnectionString;
+                status.BackColor = Color.Green;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+                connection = null;
+                status.BackColor = Color.Red;
+                return;
+            }            
+        }
+
+        public void ScanFile(string path, string diagnose="Меланома")
+        {
+            Bitmap image = new Bitmap(path);
+            /*BitmapData bmpData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, image.PixelFormat);
+            IntPtr ptr = bmpData.Scan0;
+            int bytes = Math.Abs(bmpData.Stride) * image.Height;
+            byte[] rgbValues = new byte[bytes];
+            Marshal.Copy(ptr, rgbValues, 0, bytes);*/
+
+            dbCommand.Connection = connection;
+            string query = "INSERT INTO Exsamples (Path, Diagnose) VALUES ('" + path + "', '" + diagnose + "')";
+            dbCommand.CommandText = query;
+            dbCommand.ExecuteNonQuery();
+
+            //Пока не вспомнил как с маршал работать
+            /*for (int counter = 2; counter < rgbValues.Length; counter += 3)
+            {                          
+                if(rgbValues[counter-2] == 255 && rgbValues[counter-1] == 255 && rgbValues[counter] == 255)
+                {
+                    continue;
+                }
+                else
+                {
+                    addData(rgbValues[counter - 2], rgbValues[counter - 1], rgbValues[counter], path, diagnose, , );
+                }
+            }
+            image.UnlockBits(bmpData);*/
+
+                    for (int i = 0; i < image.Width; ++i)
+                    {
+                        for (int j = 0; j < image.Height; ++j)
+                        {
+                            if (image.GetPixel(i, j) == Color.White)
+                                continue;
+                            else
+                                addData(image.GetPixel(i, j).R, image.GetPixel(i, j).G, image.GetPixel(i, j).B, path, diagnose, i, j);
+                        }
+                    }
+        }
+
+        void addData(byte R, byte G, byte B, string path, string diagnose, int x, int y) //Заполнение информации о пикселях изображений
+        {
+            string query = "INSERT INTO Spot_info (ID_photo, X_point, Y_point, R, G, B) "
+                + "VALUES ('" + path + "', " + x + ", " + y + ", " + R + ", " + G + ", " + B + ")";
+            dbCommand = new OleDbCommand(query, connection);
+            dbCommand.ExecuteNonQuery();
+        }
+
+        bool checkPath(string path) //True - есть запись, False - нет
+        {
+            string query = "SELECT * FROM Exsamples WHERE Path='" + path + "'";
+            dbCommand = new OleDbCommand(query, connection);
+            OleDbDataReader reader = dbCommand.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                reader.Close();
+                return true;
+            }
+            else
+            {
+                reader.Close();
+                return false;
+            }
+        }
+
+        private void disconnect_Click(object sender, EventArgs e) //Сброс соединения с БД
+        {
+            connectionString = "";
+            try
+            {
+                connection.Close();
+                connect_status.BackColor = Color.Red;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }            
+        }
+
+        private void default_connection_Click(object sender, EventArgs e) //Восстановление стандартного соединения
+        {
+            connection.Close();
+            makeConnection(connectionStringDefault, connect_status);
+        }
+
+        private void fillListbox()
+        {
+            exsamplesPath = GetPath();
+        }
+
+        public string[] GetPath() //Заполняет листбокс именами экземпляров из бд и возвращает список имён файлов
+        {
+            string query = "SELECT * FROM Exsamples";
+            string querycount = "SELECT count(*) as RowCount FROM Exsamples";
+            OleDbCommand command = new OleDbCommand(querycount, connection);
+            OleDbDataReader reader = command.ExecuteReader();
+            reader.Read();
+            var rowCount = (int)reader["RowCount"];
+
+            command = new OleDbCommand(query, connection);
+            reader = command.ExecuteReader();
+            int count = 0;
+            string[] ret = new string[rowCount];
+            listBox1.Items.Clear();
+
+            try
+            {
+                if (rowCount == 0)
+                {
+                    listBox1.Items.Add("База данных пуста");
+                }
+                else
+                {
+                    while (reader.Read())
+                    {
+                        listBox1.Items.Add('\\' + reader["Path"].ToString());
+                        ret[count] = '\\' + reader["Path"].ToString();
+                        count++;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+            reader.Close();
+            return ret;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //Старт программы
+        {
+            connectionStringDefault = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source='" + progpath.Replace("Debug\\", "Release") + @"\base.mdb'";
+            connectionString = connectionStringDefault;
+            connection = new OleDbConnection(connectionStringDefault);
+            textBox1.Text = connectionStringDefault;
+            makeConnection(textBox1.Text, connect_status);
+
+            fillListbox();
+        }
+
+        private void очиститьВсёToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dbCommand.Connection = connection;
+            try
+            {
+                dbCommand.CommandText = "DELETE FROM Spot_info";
+                dbCommand.ExecuteNonQuery();
+                dbCommand.CommandText = "DELETE FROM Exsamples";
+                dbCommand.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+            fillListbox();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) //клик по листбоксу
+        {
+            //MessageBox.Show(progpath.ToString() + listBox1.Items[listBox1.SelectedIndex].ToString(), "Уведомление");
+            try
+            {
+                pictureBox1.Image = new Bitmap(progpath + listBox1.Items[listBox1.SelectedIndex]);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+                return;
+            }
+            string newpath = listBox1.Items[listBox1.SelectedIndex].ToString().Substring(1);
+            //MessageBox.Show(newpath, "newpath");
+            string xyquery = @"SELECT MAX(X_point) AS max_x, MAX(Y_point) AS max_y FROM Spot_info WHERE ID_photo='" + newpath + "'";
+            OleDbCommand command = new OleDbCommand(xyquery, connection);
+            OleDbDataReader reader = command.ExecuteReader();
+
+            reader.Read();
+            var x = (int)reader["max_x"];
+            var y = (int)reader["max_y"];
+            //MessageBox.Show("X:" + x + " Y:" + y);
+            Bitmap fromBase = new Bitmap(x+1, y+1);
+
+            string query = @"SELECT * FROM Spot_info WHERE ID_photo='" + newpath + "'";
+            command = new OleDbCommand(query, connection);
+            reader = command.ExecuteReader();
+            
+
+            while (reader.Read())
+            {
+                try
+                {
+                    fromBase.SetPixel((int)reader["X_point"], (int)reader["Y_point"], Color.FromArgb(255, (int)reader["R"], (int)reader["G"], (int)reader["B"]));
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message + "\r\n" + "X:" + (int)reader["X_point"] + "\r\n" + "Y:" + (int)reader["Y_point"], "Ошибка присвоения цвета");
+                }
+            }
+            pictureBox2.Image = fromBase;
+        }
+
+        private void фотоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.SelectedPath = progpath.Replace("Debug\\", "Release");
+                if (dialog.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                else
+                {
+                    path = dialog.SelectedPath;
+                }
+            }
+
+            string[] allfiles = Directory.GetFiles(path);
+            progressBar1.Maximum = allfiles.Length;
+            progressBar1.Value = 0;
+
+            foreach (string filename in allfiles)
+            {
+                var dn = Path.GetDirectoryName(filename);
+                dn = dn.Substring(dn.LastIndexOf('\\') + 1);
+                if (!checkPath(Path.Combine(dn, Path.GetFileName(filename)))) //Если нет записи о изображении - добавляем, иначе скип
+                {
+                    ScanFile(Path.Combine(dn, Path.GetFileName(filename))); //Вызов функции обработки изображений в папке                    
+                    progressBar1.Value++;
+                }
+            }
+            fillListbox();
+        }
+    }
+}
